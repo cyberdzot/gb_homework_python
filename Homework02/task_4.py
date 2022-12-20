5012186546854e17f2cf5a8245fa474dbc9b4a55
Vovqa <vovan102vb@gmail.com>
2022-12-20 22:13:47 +0300

    Added tasks #3 and #4

--- Homework02/task_4.py
+++ Homework02/task_4.py
@@ -1,7 +1,11 @@
 
 # Требуется посчитать сумму чётных чисел, расположенных между числами 1 и N включительно.
 
+def get_summ(num):
+    sum = 0
+    for i in range(2, num+1, 2):
+        sum += i
+    return sum
 
-
-
-
+number = int(input('Введите число(от 2):'))
+print(f'Сумма чётных чисел: {get_summ(number)}')
