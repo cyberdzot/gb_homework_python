a34c3c50f24fd43b469d4cb26f0babf4a837c35b
Vovqa <vovan102vb@gmail.com>
2022-12-27 20:22:10 +0300

    Added task #1 for HW04

--- Homework04/task_1.py
+++ Homework04/task_1.py
@@ -3,3 +3,17 @@
 # * Вам необходимо вывести число Пи с той точностью знаков после запятой,
 # * сколько указал пользователь(БЕЗ round())
 
+
+from math import pi
+
+
+def pi_round(count):
+    if count < 1:
+        return 3
+    else:
+        return float(str(pi)[:2+count])
+
+
+count = input('Введите целое число: ')
+count = int(count)
+print(pi_round(count))
