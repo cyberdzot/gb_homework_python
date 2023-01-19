16f1a77de43f81024f6e798652946415de9b1ec9
Vovqa <vovan102vb@gmail.com>
2023-01-19 22:57:04 +0300

    Add tasks 1-3 for HW05

--- Homework05/task_2.py
+++ Homework05/task_2.py
@@ -1,2 +1,97 @@
 
 # * Создайте программу для игры в ""Крестики-нолики"" человек vs человек.
+
+
+EMPTY = ' '
+
+gamer = "X"
+field_id = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+field_value = [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY]
+
+
+def game_show(field, message='Игра начинается!'):
+    # спрячем мусор в терминале :)
+    print("\033[H\033[J")
+
+    print('\n')
+    print(message)
+    print('\n')
+    print(f'\t{field_id[0]}    |  {field_id[1]}  |    {field_id[2]}')
+    print(f'\t  {field[0]}  |  {field[1]}  |  {field[2]}')
+    print('\t_____|_____|_____')
+    print(f'\t{field_id[3]}    |  {field_id[4]}  |    {field_id[5]}')
+    print(f'\t  {field[3]}  |  {field[4]}  |  {field[5]}')
+    print('\t_____|_____|_____')
+    print(f'\t{field_id[6]}    |  {field_id[7]}  |    {field_id[8]}')
+    print(f'\t  {field[6]}  |  {field[7]}  |  {field[8]}')
+    print('\t     |     |')
+    print('\n')
+
+
+def game_update(number, option):
+    global field_value
+    global field_id
+
+    field_value[number - 1] = option
+    field_id[number - 1] = EMPTY
+
+
+def get_field_value(cell, _id):
+    return cell[_id - 1]
+
+
+def change_player():
+    global gamer
+
+    if gamer == "X":
+        gamer = "O"
+    else:
+        gamer = "X"
+
+
+def play():
+    global gamer
+    global field_value
+
+    game_show(field_value)
+
+    while field_value.count(EMPTY) != 0:
+        number = input()
+        number = int(number)
+
+        if number == 0:
+            # выход из игры досрочно!
+            fstr = f'Игрок "{gamer}" сдался, игра окончена!'
+            game_show(field_value, fstr)
+            return
+        elif 0 < number < 10:
+            if get_field_value(field_value, number) != EMPTY:
+                fstr = f'Ход игрока "{gamer}"! Подсказка: выбранная ячейка уже занята, выберите другую.'
+                game_show(field_value, fstr)
+            else:
+                game_update(number, gamer)
+                change_player()
+                fstr = f'Ход игрока "{gamer}"! Подсказка: чтобы покинуть игру введите цифру: 0.'
+                game_show(field_value, fstr)
+        else:
+            fstr = f'Ход игрока "{gamer}"! Введена не существующая ячейка!'
+            game_show(field_value, fstr)
+
+    # ! тут результаты надо посчитать и прописать победа чья то или ничья
+    # gamer = get_winner()
+    # fstr = f'Победил игрок "{gamer}"! Игра окончена!'
+    # game_show(field_value, fstr)
+
+
+# ! дописать надо будет
+# def get_winner():
+#     global gamer
+#     global field_value
+
+#     options = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+#     for element in field_value:
+#         print(element)
+#     return
+
+
+play()
--- Homework05/task_3.py
+++ Homework05/task_3.py
@@ -6,3 +6,76 @@
 # ....
 # stroka = "3a4b2c3b"
 # Вывод: stroka = "aaabbbbccbbb"
+
+# "w+" сообщает, что запись будет осуществляться в новый файл. Если он существует, то новое содержимое нужно записать поверх уже существующего.
+# Если же вместо этого использовать параметр "w", тогда файл будет создан только в том случае, если он не существовал до этого.
+# Если нужно добавить новые данные в файл, тогда вместо "w+" нужно просто использовать параметр "a+".
+# Однако в таком случае не будет добавлена новая строка, поэтому важно не забыть использовать символ \n.
+
+
+DATA_FOR_TEST = "aaabbbbccbbbx"
+ORIGINAL_FILE_DIR = "original.txt"
+ENCODE_FILE_DIR = "encode.txt"
+DECODE_FILE_DIR = "decode.txt"
+
+
+# создадим файл
+def create_file_txt(_dir, data):
+    my_file = open(_dir, "w+")
+    my_file.write(data)
+    my_file.close()
+
+
+# считаем файл
+def read_file_txt(_dir):
+    my_file = open(_dir)
+    output = my_file.read()
+    my_file.close()
+    return output
+
+
+def encode_data(data):
+    new_data = ''
+    char_old = data[0]
+    count = 1
+
+    _len = len(data)
+    last_index = _len - 1
+
+    for i in range(1, _len):
+        char_new = data[i]
+
+        if char_old != char_new:
+            new_data = new_data + str(count) + char_old
+            char_old = char_new
+            count = 1
+        else:
+            count += 1
+
+        if last_index == i:
+            new_data = new_data + str(count) + char_old
+
+    return new_data
+
+
+def decode_data(data):
+    new_data = ''
+
+    last_index = len(data) - 1
+
+    for i in range(0, last_index, +2):
+        for j in range(int(data[i])):
+            new_data = new_data + data[i + 1]
+    return new_data
+
+
+create_file_txt(ORIGINAL_FILE_DIR, DATA_FOR_TEST)
+data_for_encode = read_file_txt(ORIGINAL_FILE_DIR)
+
+en_data = encode_data(data_for_encode)
+
+create_file_txt(ENCODE_FILE_DIR, en_data)
+data_for_decode = read_file_txt(ENCODE_FILE_DIR)
+
+de_data = decode_data(data_for_decode)
+create_file_txt(DECODE_FILE_DIR, de_data)
